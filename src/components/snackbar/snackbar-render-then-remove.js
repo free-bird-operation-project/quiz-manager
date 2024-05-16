6e1e4e9abaa9f76f4544bcb94359eca600374ab0
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-05-16 17:13:14 +0800

    feat: add render then remove snackbar function
    This feature introduces a new function for rendering a snackbar message
    and then automatically removing it after a certain duration. The
    function combines the rendering and removal processes into a single
    action, enhancing usability and reducing complexity.

---
+++ src/components/snackbar/snackbar-render-then-remove.js
@@ -0,0 +1,56 @@
+'use strict'
+
+import { setAttributes } from '../../utilities/components/set-attributes'
+import { isConfigVerified } from '../../utilities/config/config-verifier'
+
+function renderSnackbar(config) {
+	if (!isConfigVerified('snackbar', config)) return
+
+	const { message } = config
+
+	return manageSnackbar().displaySnackbar(message)
+}
+
+function manageSnackbar() {
+	const snackbar_queue = []
+
+	function displaySnackbar(message) {
+		const is_snackbar = document.getElementById('snackbar')
+
+		if (is_snackbar) {
+			snackbar_queue.push(message)
+			return
+		}
+
+		const SNACKBAR = createSnackbar(message)
+		document.body.appendChild(SNACKBAR)
+
+		removeSnackbar(SNACKBAR)
+	}
+
+	function createSnackbar(message) {
+		const SNACKBAR = document.createElement('div')
+		setAttributes(SNACKBAR, {
+			class: 'snackbar',
+			id: 'snackbar'
+		})
+		SNACKBAR.textContent = message
+
+		return SNACKBAR
+	}
+
+	function removeSnackbar(SNACKBAR) {
+		setTimeout(() => {
+			SNACKBAR.remove()
+
+			if (snackbar_queue.length > 0) {
+				const next_message = snackbar_queue.shift()
+				displaySnackbar(next_message)
+			}
+		}, 3000)
+	}
+
+	return { displaySnackbar }
+}
+
+export { renderSnackbar }
