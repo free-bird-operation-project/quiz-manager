fb28006e6e51b95c98b70aabc147db49a7c04d27
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-05-16 09:20:15 +0800

    style: remove semicolons
    Removed semicolons for consistency with formatting guidelines.

--- src/utilities/config/has-valid-attributes.js
+++ src/utilities/config/has-valid-attributes.js
@@ -1,38 +1,38 @@
-'use strict';
+'use strict'
 
 function hasValidAttributes(component, config) {
 	const LIST_OF_VALID_ATTRIBUTES = [
 		{ button: ['class_name', 'id', 'text', 'events', 'icon', 'type'] },
 		{ tab: ['buttons'] },
 		{ snackbar: ['message'] }
-	];
+	]
 
 	const VALID_COMPONENTS = LIST_OF_VALID_ATTRIBUTES.find((list_object) =>
 		list_object.hasOwnProperty(component)
-	);
+	)
 
 	if (!VALID_COMPONENTS) {
-		console.error(`The ${component} is not found in the list.`);
-		return false;
+		console.error(`The ${component} is not found in the list.`)
+		return false
 	}
 
-	const VALID_ATTRIBUTES = VALID_COMPONENTS[`${component}`];
-	const CONFIG_KEYS = Object.keys(config);
+	const VALID_ATTRIBUTES = VALID_COMPONENTS[`${component}`]
+	const CONFIG_KEYS = Object.keys(config)
 	const INVALID_KEYS = CONFIG_KEYS.filter(
 		(KEY) => !VALID_ATTRIBUTES.includes(KEY)
-	);
-	let flag = true;
+	)
+	let flag = true
 
 	if (INVALID_KEYS.length > 0) {
 		INVALID_KEYS.forEach((INVALID_KEY) => {
 			console.error(
 				`The '${INVALID_KEY}' is not valid attribute for '${component}'.`
-			);
-			flag = false;
-		});
+			)
+			flag = false
+		})
 	}
 
-	return flag;
+	return flag
 }
 
-export { hasValidAttributes };
+export { hasValidAttributes }
--- src/utilities/config/has-valid-types.js
+++ src/utilities/config/has-valid-types.js
@@ -1,13 +1,13 @@
-'use strict';
+'use strict'
 
 function hasValidTypes(config) {
 	if (!config || config === undefined || Object.keys(config).length === 0) {
-		console.error("Make sure the 'config' is not empty.");
-		return false;
+		console.error("Make sure the 'config' is not empty.")
+		return false
 	}
 
-	const { id, class_name, text, icon, type, events, buttons } = config;
-	let flag = true;
+	const { id, class_name, text, icon, type, events, buttons } = config
+	let flag = true
 	const LIST_OF_VALID_TYPES = [
 		{
 			string: {
@@ -24,63 +24,63 @@ function hasValidTypes(config) {
 				buttons: buttons
 			}
 		}
-	];
+	]
 
 	for (const VALID_TYPES_OBJECT of LIST_OF_VALID_TYPES) {
-		validateValidTypes(VALID_TYPES_OBJECT);
+		validateValidTypes(VALID_TYPES_OBJECT)
 	}
 
 	function validateValidTypes(VALID_TYPES_OBJECT) {
 		for (const TYPE in VALID_TYPES_OBJECT) {
-			const LIST_OF_ATTRIBUTES = VALID_TYPES_OBJECT[TYPE];
-			validateAttributesOfType(TYPE, LIST_OF_ATTRIBUTES);
+			const LIST_OF_ATTRIBUTES = VALID_TYPES_OBJECT[TYPE]
+			validateAttributesOfType(TYPE, LIST_OF_ATTRIBUTES)
 		}
 	}
 
 	function validateAttributesOfType(TYPE, LIST_OF_ATTRIBUTES) {
 		for (const ATTRIBUTE in LIST_OF_ATTRIBUTES) {
 			if (TYPE === 'array') {
-				validateArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES);
+				validateArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES)
 			} else {
-				validateNonArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES, TYPE);
+				validateNonArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES, TYPE)
 			}
 		}
 	}
 
 	function validateArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES) {
-		const IS_NOT_ARRAY = !Array.isArray(LIST_OF_ATTRIBUTES[ATTRIBUTE]);
-		const IS_UNDEFINED = LIST_OF_ATTRIBUTES[ATTRIBUTE] !== undefined;
+		const IS_NOT_ARRAY = !Array.isArray(LIST_OF_ATTRIBUTES[ATTRIBUTE])
+		const IS_UNDEFINED = LIST_OF_ATTRIBUTES[ATTRIBUTE] !== undefined
 
 		if (IS_NOT_ARRAY && IS_UNDEFINED) {
-			let actual_type = getActualType(LIST_OF_ATTRIBUTES[ATTRIBUTE]);
+			let actual_type = getActualType(LIST_OF_ATTRIBUTES[ATTRIBUTE])
 
 			console.error(
 				`The attribute '${ATTRIBUTE}' is '${actual_type}' not a type of 'array'.`
-			);
-			flag = false;
+			)
+			flag = false
 		}
 	}
 
 	function validateNonArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES, TYPE) {
-		const IS_NOT_TYPE = typeof LIST_OF_ATTRIBUTES[ATTRIBUTE] !== TYPE;
-		const IS_UNDEFINED = LIST_OF_ATTRIBUTES[ATTRIBUTE] !== undefined;
+		const IS_NOT_TYPE = typeof LIST_OF_ATTRIBUTES[ATTRIBUTE] !== TYPE
+		const IS_UNDEFINED = LIST_OF_ATTRIBUTES[ATTRIBUTE] !== undefined
 
 		if (IS_NOT_TYPE && IS_UNDEFINED) {
-			let actual_type = getActualType(LIST_OF_ATTRIBUTES[ATTRIBUTE]);
+			let actual_type = getActualType(LIST_OF_ATTRIBUTES[ATTRIBUTE])
 
 			console.error(
 				`The attribute '${ATTRIBUTE}' is '${actual_type}' not a type of '${TYPE}'.`
-			);
-			flag = false;
+			)
+			flag = false
 		}
 	}
 
 	function getActualType(attribute) {
-		if (Array.isArray(attribute)) return 'array';
-		return typeof attribute;
+		if (Array.isArray(attribute)) return 'array'
+		return typeof attribute
 	}
 
-	return flag;
+	return flag
 }
 
-export { hasValidTypes };
+export { hasValidTypes }
