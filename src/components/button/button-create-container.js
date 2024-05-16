05f2b68f2c6558bef58136c363f82791027d17e5
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-05-16 08:45:38 +0800

    feat: implement button container creation function
    This function creates a container element for a button, allowing the
    inclusion of an icon, text, and various attributes such as ID, class
    name, and event listeners. It uses helper functions from the utilities
    module to set attributes and events for the container element.

---
+++ src/components/button/button-create-container.js
@@ -0,0 +1,34 @@
+'use strict'
+
+import { setAttributes } from '../../utilities/components/set-attributes'
+import { setEvents } from '../../utilities/components/set-events'
+
+function createContainer(icon, text, id, class_name, events, type) {
+	const BUTTON = document.createElement('div')
+	setAttributes(BUTTON, {
+		id: `button-${id}`,
+		class: `${class_name} button ${type}-button`.trim()
+	})
+
+	if (icon) {
+		const ICON_WRAPPER = document.createElement('div')
+
+		setAttributes(ICON_WRAPPER, {
+			'data-lucide': icon
+		})
+
+		BUTTON.appendChild(ICON_WRAPPER)
+	}
+
+	if (text) {
+		const TEXT = document.createElement('p')
+		TEXT.textContent = text
+
+		BUTTON.appendChild(TEXT)
+	}
+
+	setEvents(events)
+	return BUTTON
+}
+
+export { createContainer }
