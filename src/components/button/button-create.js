28ec926a000a1654cb122c952db25ea063774e75
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-05-16 08:45:37 +0800

    feat: implement button creation function
    This function creates a button element based on the provided
    configuration object. It checks if the configuration is verified before
    creating the button. If the configuration is not verified, the function
    returns undefined. The button type determines the style of the button:
    rounded-square, slab, or transparent.

---
+++ src/components/button/button-create.js
@@ -0,0 +1,44 @@
+'use strict'
+
+import { createContainer } from './button-create-container'
+import { isConfigVerified } from '../../utilities/config/config-verifier'
+
+function createButton(config) {
+	if (!isConfigVerified(config)) return
+	const { type, id, class_name, icon, text, events } = config
+
+	let button
+	if (!events) console.error('Button is useless without events.')
+
+	switch (type) {
+		case 'rounded-square':
+			text = undefined
+			button = createContainer(
+				icon,
+				text,
+				id,
+				class_name,
+				events,
+				'rounded-square'
+			)
+			break
+		case 'slab':
+			if (!text && !icon) return
+			button = createContainer(icon, text, id, class_name, events, 'slab')
+			break
+		default:
+			if (!text && !icon) return
+			button = createContainer(
+				icon,
+				text,
+				id,
+				class_name,
+				events,
+				'transparent'
+			)
+	}
+
+	return button
+}
+
+export { createButton }
