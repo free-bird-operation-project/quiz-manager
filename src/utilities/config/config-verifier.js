0ca369b130706ee888b4cd5ac8a2cccac9880363
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-05-16 08:50:02 +0800

    fix: add element to config verifier parameters
    To ensure the config is associated with the correct element, include the
    element as part of the parameter. This adjustment enhances security and
    ensures proper configuration validation.

--- src/utilities/config/config-verifier.js
+++ src/utilities/config/config-verifier.js
@@ -3,13 +3,15 @@
 import { hasValidAttributes } from './has-valid-attributes'
 import { hasValidTypes } from './has-valid-types'
 import { isConfig } from './is-config'
+import { isElement } from './is-element'
 
-function isConfigVerified(config) {
+function isConfigVerified(element, config) {
+	const IS_ELEMENT = isElement(element)
 	const IS_CONFIG = isConfig(config)
 	const HAS_VALID_ATTRRIBUTES = hasValidAttributes(config)
 	const HAS_VALID_TYPES = hasValidTypes()
 
-	return IS_CONFIG && HAS_VALID_ATTRRIBUTES && HAS_VALID_TYPES
+	return IS_ELEMENT && IS_CONFIG && HAS_VALID_ATTRRIBUTES && HAS_VALID_TYPES
 }
 
 export { isConfigVerified }
