8823de49d5738bd37574fbbd51cfcfcef7bc6a33
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-05-16 09:56:34 +0800

    refactor: move helper function outside main function
    Nested functions can clutter the main function. Moved the helper
    function outside the main function for improved readability and
    maintainability.

--- src/utilities/config/has-valid-types.js
+++ src/utilities/config/has-valid-types.js
@@ -27,60 +27,70 @@ function hasValidTypes(config) {
 	]
 
 	for (const VALID_TYPES_OBJECT of LIST_OF_VALID_TYPES) {
-		validateValidTypes(VALID_TYPES_OBJECT)
+		flag = validateValidTypes(VALID_TYPES_OBJECT, flag)
 	}
 
-	function validateValidTypes(VALID_TYPES_OBJECT) {
-		for (const TYPE in VALID_TYPES_OBJECT) {
-			const LIST_OF_ATTRIBUTES = VALID_TYPES_OBJECT[TYPE]
-			validateAttributesOfType(TYPE, LIST_OF_ATTRIBUTES)
-		}
-	}
-
-	function validateAttributesOfType(TYPE, LIST_OF_ATTRIBUTES) {
-		for (const ATTRIBUTE in LIST_OF_ATTRIBUTES) {
-			if (TYPE === 'array') {
-				validateArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES)
-			} else {
-				validateNonArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES, TYPE)
-			}
-		}
-	}
-
-	function validateArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES) {
-		const IS_NOT_ARRAY = !Array.isArray(LIST_OF_ATTRIBUTES[ATTRIBUTE])
-		const IS_UNDEFINED = LIST_OF_ATTRIBUTES[ATTRIBUTE] !== undefined
-
-		if (IS_NOT_ARRAY && IS_UNDEFINED) {
-			let actual_type = getActualType(LIST_OF_ATTRIBUTES[ATTRIBUTE])
-
-			console.error(
-				`The attribute '${ATTRIBUTE}' is '${actual_type}' not a type of 'array'.`
-			)
-			flag = false
-		}
-	}
-
-	function validateNonArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES, TYPE) {
-		const IS_NOT_TYPE = typeof LIST_OF_ATTRIBUTES[ATTRIBUTE] !== TYPE
-		const IS_UNDEFINED = LIST_OF_ATTRIBUTES[ATTRIBUTE] !== undefined
-
-		if (IS_NOT_TYPE && IS_UNDEFINED) {
-			let actual_type = getActualType(LIST_OF_ATTRIBUTES[ATTRIBUTE])
-
-			console.error(
-				`The attribute '${ATTRIBUTE}' is '${actual_type}' not a type of '${TYPE}'.`
-			)
-			flag = false
-		}
-	}
-
-	function getActualType(attribute) {
-		if (Array.isArray(attribute)) return 'array'
-		return typeof attribute
-	}
-
+	console.log(flag)
 	return flag
+}
+
+function validateValidTypes(VALID_TYPES_OBJECT, flag) {
+	for (const TYPE in VALID_TYPES_OBJECT) {
+		const LIST_OF_ATTRIBUTES = VALID_TYPES_OBJECT[TYPE]
+		flag = validateAttributesOfType(TYPE, LIST_OF_ATTRIBUTES, flag)
+	}
+	return flag
+}
+
+function validateAttributesOfType(TYPE, LIST_OF_ATTRIBUTES, flag) {
+	for (const ATTRIBUTE in LIST_OF_ATTRIBUTES) {
+		if (TYPE === 'array') {
+			flag = validateArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES, flag)
+		} else {
+			flag = validateNonArrayAttribute(
+				ATTRIBUTE,
+				LIST_OF_ATTRIBUTES,
+				TYPE,
+				flag
+			)
+		}
+	}
+	return flag
+}
+
+function validateArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES, flag) {
+	const IS_NOT_ARRAY = !Array.isArray(LIST_OF_ATTRIBUTES[ATTRIBUTE])
+	const IS_UNDEFINED = LIST_OF_ATTRIBUTES[ATTRIBUTE] !== undefined
+
+	if (IS_NOT_ARRAY && IS_UNDEFINED) {
+		let actual_type = getActualType(LIST_OF_ATTRIBUTES[ATTRIBUTE])
+
+		console.error(
+			`The attribute '${ATTRIBUTE}' is '${actual_type}' not a type of 'array'.`
+		)
+		flag = false
+	}
+	return flag
+}
+
+function validateNonArrayAttribute(ATTRIBUTE, LIST_OF_ATTRIBUTES, TYPE, flag) {
+	const IS_NOT_TYPE = typeof LIST_OF_ATTRIBUTES[ATTRIBUTE] !== TYPE
+	const IS_UNDEFINED = LIST_OF_ATTRIBUTES[ATTRIBUTE] !== undefined
+
+	if (IS_NOT_TYPE && IS_UNDEFINED) {
+		let actual_type = getActualType(LIST_OF_ATTRIBUTES[ATTRIBUTE])
+
+		console.error(
+			`The attribute '${ATTRIBUTE}' is '${actual_type}' not a type of '${TYPE}'.`
+		)
+		flag = false
+	}
+	return flag
+}
+
+function getActualType(attribute) {
+	if (Array.isArray(attribute)) return 'array'
+	return typeof attribute
 }
 
 export { hasValidTypes }
