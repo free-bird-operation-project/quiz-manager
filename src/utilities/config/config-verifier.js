149544ba4602b29d57c6ee1aef4754d550d902bc
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-05-16 08:50:08 +0800

    refactor: add missing parameter to config verifier
    Include the previously forgotten parameter in the config verifier to
    ensure proper configuration verification and functionality.

--- src/utilities/config/config-verifier.js
+++ src/utilities/config/config-verifier.js
@@ -8,8 +8,8 @@ import { isElement } from './is-element'
 function isConfigVerified(element, config) {
 	const IS_ELEMENT = isElement(element)
 	const IS_CONFIG = isConfig(config)
-	const HAS_VALID_ATTRRIBUTES = hasValidAttributes(config)
-	const HAS_VALID_TYPES = hasValidTypes()
+	const HAS_VALID_ATTRRIBUTES = hasValidAttributes(element, config)
+	const HAS_VALID_TYPES = hasValidTypes(config)
 
 	return IS_ELEMENT && IS_CONFIG && HAS_VALID_ATTRRIBUTES && HAS_VALID_TYPES
 }
