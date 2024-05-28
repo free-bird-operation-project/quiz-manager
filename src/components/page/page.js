bd265c59bd24fa7b2a2efc6627858512e65cc242
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-05-28 15:03:49 +0800

    feat: implement Page component for page creation and removal
    Implement the Page component to handle page creation and removal based
    on the provided configuration. The Page class consists of a constructor
    method that initializes the configuration, a create method to create
    pages, and a remove method to remove pages. This implementation ensures
    that the provided configuration is verified before creating or removing
    pages.

---
+++ src/components/page/page.js
@@ -0,0 +1,22 @@
+'use strict'
+
+import { isConfigVerified } from '../../utilities/config/config-verifier'
+import { createPage } from './page-create'
+import { removePage } from './page-remove'
+
+class Page {
+	constructor(config) {
+		this.name = 'page'
+		this.config = isConfigVerified(this.name, config) ? config : {}
+	}
+
+	create() {
+		createPage(this.config, this.remove.bind(this))
+	}
+
+	remove() {
+		removePage(this.config)
+	}
+}
+
+export { Page }
