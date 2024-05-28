dbe79674def2d73bb0704f06a33d931f5a32584f
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-05-28 15:04:57 +0800

    feat: implement page removal function
    This function removes a page element from the DOM based on the provided
    configuration. It utilizes strict mode for improved code quality and
    reliability.

---
+++ src/components/page/page-remove.js
@@ -0,0 +1,15 @@
+'use strict'
+
+import { isConfigVerified } from '../../utilities/config/config-verifier'
+
+function removePage(config) {
+	if (!isConfigVerified('page', config)) return
+
+	const { id } = config
+	const PAGE = document.createElement(id)
+
+	if (!PAGE) return
+	PAGE.remove()
+}
+
+export { removePage }
