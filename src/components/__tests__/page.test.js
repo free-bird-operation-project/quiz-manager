a1e3a4eea6a5557b4a0f00557380f28d79d8636d
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-06-20 12:53:49 +0800

    fix: change class_name to class when setting attribute
    Adjust the attribute setting logic to use `class` instead of
    `class_name` for setting the class attribute. This ensures compatibility
    with standard HTML attributes.
    The test ensures everything worked properly.

--- src/components/__tests__/page.test.js
+++ src/components/__tests__/page.test.js
@@ -64,7 +64,7 @@ describe('Page', () => {
 					.getElementsByClassName('page-header')
 			)
 			headers.forEach((header) => {
-				expect(header.querySelector('#exit-button-page-6')).not.toBeNull()
+				expect(header.querySelector('#button-exit-page-6')).not.toBeNull()
 			})
 		})
 
@@ -95,7 +95,7 @@ describe('Page', () => {
 		it('should create with elements added in config', () => {
 			let button_1 = new Button({
 				icon: 'badge',
-				id: 'add-quiz-3',
+				id: 'add-quiz-1',
 				text: 'Badge',
 				class_name: 'maker-button',
 				events: [
@@ -106,12 +106,12 @@ describe('Page', () => {
 						}
 					}
 				]
-			}).create()
+			})
 
 			let button_2 = new Button({
 				icon: 'cookie',
 				text: 'Cookie',
-				id: 'add-quiz-4',
+				id: 'add-quiz-2',
 				class_name: 'maker-button',
 				events: [
 					{
@@ -121,19 +121,19 @@ describe('Page', () => {
 						}
 					}
 				]
-			}).create()
+			})
 
 			let config_modal = {
 				id: 'modal',
 				title: 'Sample Modal',
 				icon: 'heart',
-				buttons: [button_1, button_2]
+				buttons: [button_1.create(), button_2.create()]
 			}
 
 			let button_3 = new Button({
 				icon: 'anchor',
 				text: 'Anchor',
-				id: 'add-quiz-4',
+				id: 'add-quiz-3',
 				class_name: 'maker-button',
 				events: [
 					{
@@ -148,7 +148,7 @@ describe('Page', () => {
 			let button_4 = new Button({
 				icon: 'grid-2x2-check',
 				text: 'Grid Check',
-				id: 'add-quiz-5',
+				id: 'add-quiz-4',
 				class_name: 'maker-button',
 				events: [
 					{
@@ -180,16 +180,17 @@ describe('Page', () => {
 				document.body
 					.querySelector('#page-maker')
 					.getElementsByClassName('.page-header')
-			).toBeTruthy()
+			).not.toBeNull()
 
 			const headers = Array.from(
 				document.body
 					.querySelector('#page-maker')
-					.getElementsByClassName('page-header')
+					.getElementsByClassName('.page-header')
 			)
+
 			headers.forEach((header) => {
-				expect(header.querySelector('#exit-button-page-4')).toBeNull()
-				expect(header.querySelector('#add-quiz-5-button')).not.toBeNull()
+				expect(header.getElementById('button-exit-page-4')).not.toBeNull()
+				expect(header.getElementById('button-add-quiz-4')).not.toBeNull()
 			})
 
 			expect(
@@ -205,7 +206,7 @@ describe('Page', () => {
 			)
 			bodies.forEach((body) => {
 				expect(body.querySelector('#modal')).not.toBeNull()
-				expect(body.querySelector('#add-quiz-4-button')).not.toBeNull()
+				expect(body.querySelector('#button-add-quiz-3')).not.toBeNull()
 			})
 		})
 	})
