0fe48466c3f48c170cb106f7a01ef95b61e5b12a
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-08-22 18:26:20 +0800

    refactor: overhaul Modal component
    Inherited from `BaseComponent` to maintain consistency with other
    components. Updated `create` method to use `_createContainer` and
    `_createIcon` methods for modularity. Refactored `remove` method to
    handle button instances and return a boolean indicating success.
    Simplified `modifyButtons` method by integrating ID assignment directly
    into `create`. Added detailed comments and improved method documentation.

---
+++ src/components/modal.js
@@ -0,0 +1,74 @@
+import { BaseComponent } from './base-component'
+import { sanitizeValue } from '@utilities/sanitize-value'
+
+/**
+ * Represents a Modal component.
+ */
+class Modal extends BaseComponent {
+	/**
+	 * Initializes the modal instance with the provided configuration.
+	 *
+	 * @param {Object} config - The configuration object for the modal.
+	 */
+	constructor(config) {
+		super(config)
+		this.config = sanitizeValue(config, 'object')
+	}
+
+	/**
+	 * Creates the modal element based on the provided configuration.
+	 *
+	 * @returns {HTMLElement|null} The created modal element or null if invalid configuration.
+	 */
+	create() {
+		const { id, title, icon, buttons } = this.config
+
+		const modal = this._createContainer('div', {
+			id: `modal-${id}`,
+			class: 'modal'
+		})
+
+		const titleContainer = this._createContainer('div', {
+			class: 'modal-title-container'
+		})
+		const iconHolder = this._createIcon(icon)
+		const titleText = this._createText(title)
+
+		if (iconHolder) titleContainer.appendChild(iconHolder)
+		if (titleText) titleContainer.appendChild(titleText)
+
+		modal.appendChild(titleContainer)
+
+		buttons.forEach((button, index) => {
+			button.id = `modal-button-${index}`
+			modal.appendChild(button)
+		})
+
+		return modal
+	}
+
+	/**
+	 * Removes the modal element and its buttons from the DOM.
+	 */
+	remove() {
+		const { id, buttonInstances } = this.config
+
+		let allRemoved = true
+
+		buttonInstances.forEach((buttonInstance) => {
+			try {
+				if (!buttonInstance.remove()) {
+					allRemoved = false
+				}
+			} catch (error) {
+				console.error(`Error: ${error}`)
+				allRemoved = false
+			}
+		})
+
+		const modalRemoved = this._removeById(id)
+		return allRemoved && modalRemoved
+	}
+}
+
+export { Modal }
