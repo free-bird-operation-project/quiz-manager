ca6d57d15ec647985e564920a311e5d25e45459f
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-08-22 17:37:56 +0800

    feat(webpack): add SASS support in development config
    Updated the `webpack.config.dev.js` file to include SASS support by
    modifying the module rules. The configuration now handles `.scss` and
    `.sass` files using `style-loader`, `css-loader`, and `sass-loader`.
    This allows for easier styling in the development environment.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -13,8 +13,8 @@ module.exports = merge(commonConfig, {
 	module: {
 		rules: [
 			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
+				test: /\.s[ac]ss$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader']
 			}
 		]
 	}
