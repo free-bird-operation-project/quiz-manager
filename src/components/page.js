706cc8fee57c784270940a98317d1edd86f8b71a
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-08-22 19:36:47 +0800

    refactor: overhaul Page component
    Inherited from `BaseComponent` for consistency with other components.
    Updated `create` method to use `_createContainer` for creating header
    and body sections. Integrated `Button` component directly into `create`
    method for dynamic button creation. Refactored `remove` method to use
    improved node removal logic and handle element instances. Simplified
    internal methods for creating and removing nodes, and updated attribute
    handling. Added detailed comments for improved code clarity and
    maintainability.

--- src/components/page.js
+++ src/components/page.js
@@ -1,112 +1,89 @@
-import { isConfigVerified } from '@utilities/config/config-verifier'
-import { setAttributes } from '@utilities/components/set-attributes'
+import { BaseComponent } from './base-component'
+import { sanitizeValue } from '@utilities/sanitize-value'
 import { Button } from './button'
 
-class Page {
-	#config
-
+class Page extends BaseComponent {
 	constructor(config) {
-		this.#config = isConfigVerified('page', config) ? config : {}
+		super(config)
+		this.config = sanitizeValue(config, 'object')
 	}
 
 	create() {
-		const { elements, z_index, id } = this.#config
-
-		const header_elements = this.#createNodes(elements.header)
-		const body_elements = this.#createNodes(elements.body)
-
-		const PAGE = document.createElement('div')
-		setAttributes(PAGE, {
+		const { elements, zIndex, id } = this.config
+		const page = this._createContainer('div', {
 			id: `page-${id}`,
 			class: 'page',
-			style: `z-index: ${z_index}`
+			style: `z-index: ${zIndex}`
 		})
-		const HEADER = this.#createHeader(header_elements, z_index)
-		const BODY = this.#createBody(body_elements)
-
-		PAGE.appendChild(HEADER)
-		PAGE.appendChild(BODY)
-
-		return PAGE
-	}
-
-	remove() {
-		const { id, elements } = this.#config
-		let PAGE = document.getElementById(`page-${id}`)
-
-		this.#removeNodes(elements.header)
-		this.#removeNodes(elements.body)
-
-		if (!PAGE) return
-		PAGE.remove()
-		PAGE = null
-	}
-
-	#createNodes(elements) {
-		if (!elements) return
-
-		const element_array = []
-
-		elements.forEach((element) => {
-			element_array.push(element.create())
-		})
-
-		return element_array
-	}
-
-	#createHeader(elements, z_index) {
-		const HEADER = document.createElement('div')
-		setAttributes(HEADER, {
+		const header = this._createContainer('div', {
 			class: 'page-header'
 		})
 
-		if (z_index > 5) {
-			const exit_config = {
+		const zIndexForMainPages = 10
+
+		if (zIndex > zIndexForMainPages) {
+			const exitButtonConfig = {
 				icon: 'chevron-left',
-				id: `exit-page-${z_index}`,
-				class_name: 'exit',
+				id: `exit-page-${zIndex}`,
+				className: 'exit',
 				type: 'rounded-square',
 				events: [
 					{
-						event_name: 'click',
+						type: 'click',
 						func: this.remove.bind(this)
 					}
 				]
 			}
-			const exit_button = new Button(exit_config).create()
-
-			HEADER.appendChild(exit_button)
+			const exitButton = new Button(exitButtonConfig).create()
+			header.appendChild(exitButton)
 		}
 
-		if (elements) {
-			elements.forEach((element) => {
-				HEADER.appendChild(element)
+		if (elements.header) {
+			elements.header.forEach((element) => {
+				header.appendChild(element.create())
 			})
 		}
 
-		return HEADER
-	}
-
-	#createBody(elements) {
-		const BODY = document.createElement('div')
-		setAttributes(BODY, {
+		const body = this._createContainer('div', {
 			class: 'page-body'
 		})
 
-		elements.forEach((element) => {
-			BODY.appendChild(element)
-		})
+		if (elements.body) {
+			elements.body.forEach((element) => {
+				body.appendChild(element.create())
+			})
+		}
 
-		return BODY
+		page.appendChild(header)
+		page.appendChild(body)
+
+		return page
 	}
 
-	#removeNodes(elements) {
-		if (!elements) return
+	remove() {
+		const { elements, id } = this.config
 
-		elements.forEach((element) => {
-			element.remove()
-			element = null
-		})
+		const elementId = `page-${id}`
+		const page = document.getElementById(elementId)
+
+		if (elements.header) {
+			elements['header'].forEach((element) => {
+				element.remove()
+			})
+		}
+
+		if (elements.body) {
+			elements['body'].forEach((element) => {
+				element.remove()
+			})
+		}
+
+		if (page) {
+			page.remove()
+			return true
+		}
+
+		return false
 	}
 }
 
