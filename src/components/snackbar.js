8b18b53befd0ec7fb2d8f69a7862f2615c682bd1
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-08-22 19:37:01 +0800

    docs: update components JSDoc

--- src/components/snackbar.js
+++ src/components/snackbar.js
@@ -1,23 +1,47 @@
 import { sanitizeValue } from '@utilities/sanitize-value'
 
+/**
+ * @typedef {Object} SnackbarConfig
+ * @property {string} message - The title of the snackbar.
+ */
+
+/**
+ * Represents a Snackbar component.
+ */
 class Snackbar {
 	static queue = []
 	static isDisplaying = false
 
+	/**
+	 * Initializes the snackbar instance with the provided configuration.
+	 *
+	 * @param {SnackbarConfig} config - The configuration object for the snackbar.
+	 */
 	constructor(config) {
 		this.config = sanitizeValue(config, 'object')
 	}
 
+	/**
+	 * Renders the snackbar message and removes it after displaying.
+	 */
 	renderThenRemove() {
 		const { message } = this.config
 		Snackbar.#enqueue(message)
 	}
 
+	/**
+	 * Adds a message to the snackbar queue and triggers the queue processing.
+	 *
+	 * @param {string} message - The message to be added to the queue.
+	 */
 	static #enqueue(message) {
 		Snackbar.queue.push(message)
 		Snackbar.#processQueue()
 	}
 
+	/**
+	 * Processes the snackbar queue by displaying the next message in the queue if not already displaying a message.
+	 */
 	static #processQueue() {
 		if (Snackbar.isDisplaying || Snackbar.queue.length === 0) {
 			return
@@ -28,6 +52,11 @@ class Snackbar {
 		Snackbar.#display(message)
 	}
 
+	/**
+	 * Displays a snackbar message on the screen.
+	 *
+	 * @param {string} message - The message to be displayed in the snackbar.
+	 */
 	static #display(message) {
 		const isSnackbar = document.getElementById('snackbar')
 
@@ -45,6 +74,11 @@ class Snackbar {
 		Snackbar.#remove(snackbar)
 	}
 
+	/**
+	 * Removes the snackbar element after a specified duration by animating its removal.
+	 *
+	 * @param {Element} snackbar - The snackbar element to be removed.
+	 */
 	static #remove(snackbar) {
 		let start
 		const duration = 3000
--- src/components/textarea.js
+++ src/components/textarea.js
@@ -1,12 +1,25 @@
 import { BaseComponent } from './base-component'
 import { sanitizeValue } from '@utilities/sanitize-value'
 
+/**
+ * Represents a Textarea component.
+ */
 class Textarea extends BaseComponent {
+	/**
+	 * Initializes the textarea instance with the provided configuration.
+	 *
+	 * @param {Object} config - The configuration object for the textarea.
+	 */
 	constructor(config) {
 		super(config)
 		this.config = sanitizeValue(config)
 	}
 
+	/**
+	 * Creates a textarea element based on the provided configuration.
+	 *
+	 * @returns {HTMLElement} The created textarea element.
+	 */
 	create() {
 		const { id, className, placeholder, text, readOnly, hidden } = this.config
 		const textarea = this._createContainer(
@@ -27,6 +40,11 @@ class Textarea extends BaseComponent {
 		return textarea
 	}
 
+	/**
+	 * Removes the textarea element from the DOM based on the configuration ID.
+	 *
+	 * @returns {boolean} Returns true if the textarea element was successfully removed, otherwise false.
+	 */
 	remove() {
 		const { id } = this.config
 		const elementId = `textarea-${id}`
@@ -40,6 +58,11 @@ class Textarea extends BaseComponent {
 		return false
 	}
 
+	/**
+	 * Locks the textarea element to make it read-only.
+	 *
+	 * @returns {boolean} Returns true if the textarea element was successfully locked, otherwise false.
+	 */
 	lock() {
 		const { id } = this.config
 		const elementId = `textarea-${id}`
@@ -53,6 +76,11 @@ class Textarea extends BaseComponent {
 		return false
 	}
 
+	/**
+	 * Unlocks the textarea element to make it editable.
+	 *
+	 * @returns {boolean} Returns true if the textarea element was successfully unlocked, otherwise false.
+	 */
 	unlock() {
 		const { id } = this.config
 		const elementId = `textarea-${id}`
